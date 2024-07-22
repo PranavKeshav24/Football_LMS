114f026aa07adcea2bec90e4274d39b05305ace8
Pranav Keshav <127531721+PranavKeshav24@users.noreply.github.com>
2024-07-22 20:26:46 +0530

    Revert "Added Player CRUD functionality"

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,5 @@
 import TopicsList from "@/components/TopicsList";
-import PlayersList from "@/components/PlayerList";
 
 export default function Home() {
-  return <PlayersList />;
+  return <TopicsList />;
 }
